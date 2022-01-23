cbde8e9a6794d756ae8130a32a21403d5f3a5e67
Abdul Moiz <moizhus123@gmail.com>
2022-01-23 13:02:38 +0500

    Add project files.

---
+++ Utilities_ConsoleAppNetCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Utilities_ConsoleAppNetCore.Utils;
+
+namespace Utilities_ConsoleAppNetCore
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // usages:
+      var dateTime = DateTime.Parse("2022-01-22T12:33:15.672");
+
+      long millis = dateTime.GetMillisSince1970();
+      // Result of the method GetMillisSince1970:
+      // must be equal to Javascript's 'new Date("2022-01-22T12:33:15.672").getTime();'
+    }
+  }
+}
---
+++ Utilities_ConsoleAppNetCore/Utils/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Utilities_ConsoleAppNetCore.Utils
+{
+  public static class DateTimeExtensions
+  {
+    public static readonly long TicksOffset = TimeZoneInfo.Local.GetUtcOffset(new DateTime()).Ticks;
+    public static readonly long Ticks1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
+
+    /// <summary>
+    /// Will work correctly only for the dates which are greater than 1970-01-01T00:00:00.000Z
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns></returns>
+    public static long GetMillisSince1970(this DateTime dateTime)
+    {
+      return (dateTime.Ticks - Ticks1970 - TicksOffset) / TimeSpan.TicksPerMillisecond;
+    }
+  }
+}
